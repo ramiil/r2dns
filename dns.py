07e95d037a797abdc370cb3975f2cb746f62e3d7
ramiil <ramiil@PC-04>
2017-08-24 16:20:45 +0600

    add debug info

--- dns.py
+++ dns.py
@@ -1,6 +1,17 @@
 import socket
 import sqlite3
 
+def setbit(number, index):
+  number |= (1<<index)
+  return number
+
+def unsetbit(number, index):
+  number &= ~(1<<index)
+  return number
+
+def getbit(number, index):
+  return int(number&(1<<index)!=0)
+
 class NameStoarge:
   def __init__(self, dbFile):
     self.db = sqlite3.connect(dbFile)
@@ -18,35 +29,45 @@ class NameStoarge:
       return '0.0.0.0'
 
 class DNSQuery:
-  def __init__(self, data):
+  def __init__(self, data, clientIP):
     self.data=data
     self.domain=''
 
-    tip = (ord(data[2]) >> 3) & 15   # Opcode bits
-    if tip == 0:                     # Standard query
-      ini=12
-      lon=ord(data[ini])
-      while lon != 0:
-        self.domain+=data[ini+1:ini+lon+1]+'.'
-        ini+=lon+1
-        lon=ord(data[ini])
+    if getbit(ord(data[2]), 1) == 0:	# Standard query
+      ptr=12				# Pointer to first address block length
+      length=ord(data[ptr])		# Length of first address block
+      while length != 0:
+	self.domain+=data[ptr+1:ptr+length+1]+'.'
+	ptr+=length+1
+	length=ord(data[ptr])
 
-  def answer(self, ip, clientIP):
+  def parseQuery(self):
+    print " ".join("{:02x}".format(ord(c)) for c in self.data)
+    print "ID:"+" ".join("{:02x}".format(ord(c)) for c in self.data[0:2])
+    print "FLAGS: "+" ".join("{:02x}".format(ord(c)) for c in self.data[2:4])
+    print "QueN: "+" ".join("{:02x}".format(ord(c)) for c in self.data[4:6])
+    print "AnsN: "+" ".join("{:02x}".format(ord(c)) for c in self.data[6:8])
+    print "NSN: "+" ".join("{:02x}".format(ord(c)) for c in self.data[8:10])
+    print "ARC: "+" ".join("{:02x}".format(ord(c)) for c in self.data[10:12])
+    print "ptr: "+" ".join("{:02x}".format(ord(c)) for c in self.data[12:13])
+
+  def answer(self, ip):
+    self.parseQuery()
     packet=''
     if self.domain:
-      if (ip=='255.255.255.255'):  # Mean we need return client's IP instead
-        ip=clientIP
+      if (ip=='255.255.255.255'):  		# We need return client's IP instead
+        ip=self.clientIP
       if (ip=='0.0.0.0'):
-        packet+=self.data[:2] + "\x81\x83" # NXDOMAIN
+        packet+=self.data[:2] + "\x81\x83" 	# We need return NXDOMAIN
       else:
-        packet+=self.data[:2] + "\x85\x80" # OK
-      packet+=self.data[4:6] + self.data[4:6] + '\x00\x00\x00\x00'   # Questions and Answers Counts
-      packet+=self.data[12:]                                         # Original Domain Name Question
-      packet+='\xc0\x0c'                                             # Pointer to domain name
+        packet+=self.data[:2] + "\x85\x80" 	# We need return requested domain's IP
+      packet+=self.data[4:6] + self.data[4:6] + '\x00\x00\x00\x00'   # Requests and Answers Counts
+      packet+=self.data[12:]                                         # Original Domain Name Request
+      packet+='\xc0\x0c'                                             # Pointer to domain name block start
       packet+='\x00\x01\x00\x01\x00\x00\x00\x3c\x00\x04'             # Response type, ttl and resource data length -> 4 bytes
       packet+=str.join('',map(lambda x: chr(int(x)), ip.split('.'))) # 4bytes of IP
       print ' Answer: ',self.domain,' -> ',ip
-      print " ".join("{:02x}".format(ord(c)) for c in packet)
+      #print " ".join("{:02x}".format(ord(c)) for c in packet)
     return packet
 
 udps = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -59,9 +80,9 @@ try:
     qData, clAddr = udps.recvfrom(1024)
     print "New request from ",clAddr[0]
     ipHash = socket.inet_aton(clAddr[0])
-    print map(lambda x: x.encode('hex'), ipHash)
-    dnsq=DNSQuery(qData)
-    udps.sendto(dnsq.answer(ns.getIpByName(dnsq.domain[:len(dnsq.domain)-1]), clAddr[0]), clAddr)
+    #print map(lambda x: x.encode('hex'), ipHash)
+    dnsq=DNSQuery(qData, clAddr[0])
+    udps.sendto(dnsq.answer(ns.getIpByName(dnsq.domain[:len(dnsq.domain)-1])), clAddr)
 
 except KeyboardInterrupt:
   udps.close()
